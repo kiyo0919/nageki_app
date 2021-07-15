7a69beb326351595206fb57e3b21cdae4e33d241
kiyo0919 <kakinoki0919@gmail.com>
2021-07-15 13:08:43 +0000

    [add]various functions

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   resources :posts, only: [:create, :index, :show, :edit, :update, :destroy]
+  resources :users, only: [:show, :index, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

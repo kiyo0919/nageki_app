6a4ff5ca600cb07af705188e38786e0d03edbc28
kiyo0919 <kakinoki0919@gmail.com>
2021-07-15 06:13:35 +0000

    [add]post-new-destroy function

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,36 @@
+class PostsController < ApplicationController
+  
+  def index
+    @posts = Post.all
+    @post = Post.new
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    @new_post = Post.new
+    @user = @post.user
+  end
+  
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+       redirect_to post_path(@post)
+    else
+       @posts = Post.all
+       render :index
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+  
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+end

c14517ca17545d89a20654377af5f07e26b8a2a2
kiyo0919 <kakinoki0919@gmail.com>
2021-07-15 03:53:43 +0000

    [update]sqlite version

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module NagekiApp
   class Application < Rails::Application
+    config.hosts << '56ec0d73f038438bbf4aa2fb5ffa7f42.vfs.cloud9.ap-northeast-1.amazonaws.com'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
